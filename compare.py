0ece9478c4e5def1cc4c8d448fe0ee49e014be64
Reid Draper <reiddraper@gmail.com>
2021-11-30 18:05:48 -0600

    Narrow down exception

--- compare.py
+++ compare.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 
-from hypothesis import given, settings
+from hypothesis import given, settings, seed
 import hypothesis.strategies as st
 
 def encode_field(field):
@@ -27,23 +27,9 @@ def array_to_bytes(arr):
         b += comma
     return b
 
-
-test_data = [
-    [[1, 2, 3], [1, 2, 3]],
-    [["this is a really long string"], ["short"]],
-    [["this is a really long string", 2, 3], ["short", 2, 3]],
-    [[2 ** 16], [2 ** 15]],
-    [[1, 0, 1, "prerelease-101"], [1, 0, 1, "prerelease-1000"]],
-    # this one below is one of the trickier examples
-    [["abc"], ["a", "b", "c"]],
-]
-
 positive_integer = st.integers(0, (2 ** 64)-1)
 
 text = st.text(min_size=0, max_size=128)
-
-three_part_tuple = st.tuples(positive_integer, positive_integer, text)
-two_part_tuple = st.tuples(text, positive_integer)
 
 @st.composite
 def pair_of_version_list_with_same_types(draw):
@@ -70,14 +56,3 @@ def test_compare(args):
 
 if __name__ == '__main__':
     test_compare()
-    for (a, b) in test_data:
-        control = a < b
-        candidate = array_to_bytes(a) < array_to_bytes(b)
-        if not control == candidate:
-            print('Failure!')
-            print(f'Control={control}')
-            print(f'Candidate={candidate}')
-            print(a)
-            print(b)
-            print(array_to_bytes(a))
-            print(array_to_bytes(b))
